15cd2e3ab6741b494c3497213ca54b9ddc6b2e3a
Adrian Ferrera <adrian.afergon@gmail.com>
2021-03-06 16:33:37 +0000

    🚧 define basic test before start implementation

---
+++ src/__tests__/acceptance/pokemon.controller.acceptance.ts
@@ -0,0 +1,21 @@
+import {Client, expect} from '@loopback/testlab';
+import {PokedexApplication} from '../..';
+import {setupApplication} from './test-helper';
+
+describe('PokemonController', () => {
+  let app: PokedexApplication;
+  let client: Client;
+
+  before('setupApplication', async () => {
+    ({app, client} = await setupApplication());
+  });
+
+  after(async () => {
+    await app.stop();
+  });
+
+  it('invokes GET /pokemon', async () => {
+    const res = await client.get('/pokemon').expect(200);
+    expect(res.body.pokemon).to.have.length(151);
+  });
+});
