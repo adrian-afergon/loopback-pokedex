78389af9c2436f946eafe7f82dec70bb8a200077
Adrián Ferrera González <adrian.afergon@gmail.com>
2021-03-07 13:18:53 +0000

    ✨ Feature/query all pokemon without filters (#5)
    * 🏷️ create pokemon entity based on the database schema
    * 🚧 define basic test before start implementation
    * 🩹 move to lowecase the reference to the pokemon collection
    * 🩹 update test acceptance
    * ✨ create datasource for mongodb
    * ✨ create pokemon repository using mongo datasource
    * 🩹 update model to display id, and hide internal _id
    * ✨ create controller with get method to query all the pokemon

---
+++ src/models/pokemon.model.ts
@@ -0,0 +1,111 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}, name: 'pokemon'})
+export class Pokemon extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+    required: true,
+    hidden: true,
+  })
+  _id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  classification: string;
+
+  @property({
+    type: 'array',
+    itemType: 'string',
+    required: true,
+  })
+  types: string[];
+
+  @property({
+    type: 'array',
+    itemType: 'string',
+  })
+  resistant?: string[];
+
+  @property({
+    type: 'array',
+    itemType: 'string',
+  })
+  weaknesses?: string[];
+
+  @property({
+    type: 'object',
+  })
+  weight?: object;
+
+  @property({
+    type: 'object',
+  })
+  height?: object;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  fleeRate?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  maxCP: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  maxHP: number;
+
+  @property({
+    type: 'object',
+    required: true,
+  })
+  attacks: object;
+
+  @property({
+    type: 'object',
+  })
+  evolutions?: object;
+
+  @property({
+    type: 'object',
+  })
+  evolutionRequirements?: object;
+
+  @property({
+    type: 'object',
+  })
+  previousEvolutions?: object;
+
+  [prop: string]: string | number | object | boolean | undefined;
+
+  constructor(data?: Partial<Pokemon>) {
+    super(data);
+  }
+}
+
+export interface PokemonRelations {
+  // describe navigational properties here
+}
+
+export type PokemonWithRelations = Pokemon & PokemonRelations;
