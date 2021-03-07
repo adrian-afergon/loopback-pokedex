83b61e54ae580e5a68734f193f22ada2d5c12268
Adrian Ferrera <adrian.afergon@gmail.com>
2021-03-07 14:49:26 +0000

    ✅ define acceptance test for retrieving pokemon by id

--- src/__tests__/acceptance/pokemon.controller.acceptance.ts
+++ src/__tests__/acceptance/pokemon.controller.acceptance.ts
@@ -18,4 +18,15 @@ describe('PokemonController', () => {
     const res = await client.get('/pokemon').expect(200);
     expect(res.body).to.have.length(151);
   });
+
+  it('invokes GET /pokemon/{id} existent pokemon id', async () => {
+    const pokemonId = '001';
+    const res = await client.get(`/pokemon/${pokemonId}`).expect(200);
+    expect(res.body.id).to.eql(pokemonId);
+  });
+
+  it('invokes GET /pokemon/{id} with non-existent pokemon id', async () => {
+    const pokemonId = 'non-existent-id';
+    await client.get(`/pokemon/${pokemonId}`).expect(404);
+  });
 });
