c51c4e2f5e7976ae63d4ec09ac103fcaea7045b8
Adrian Ferrera <adrian.afergon@gmail.com>
2021-03-07 16:29:27 +0000

    ♻️ regroup test to improve readability on the use cases

--- src/__tests__/acceptance/pokemon.controller.acceptance.ts
+++ src/__tests__/acceptance/pokemon.controller.acceptance.ts
@@ -14,36 +14,42 @@ describe('PokemonController', () => {
     await app.stop();
   });
 
-  it('invokes GET /pokemon', async () => {
-    const res = await client.get('/pokemon').expect(200);
-    expect(res.body).to.have.length(151);
+  describe('invokes GET /pokemon', () => {
+    it('returns the full list of pokemon', async () => {
+      const res = await client.get('/pokemon').expect(200);
+      expect(res.body).to.have.length(151);
+    });
   });
 
-  it('invokes GET /pokemon/{id} existent pokemon id', async () => {
-    const pokemonId = '001';
-    const res = await client.get(`/pokemon/${pokemonId}`).expect(200);
-    expect(res.body.id).to.eql(pokemonId);
+  describe('invokes GET /pokemon/{id}', () => {
+    it('returns the existent pokemon given an id', async () => {
+      const pokemonId = '001';
+      const res = await client.get(`/pokemon/${pokemonId}`).expect(200);
+      expect(res.body.id).to.eql(pokemonId);
+    });
+
+    it('returns not found status given a non-existent pokemon id', async () => {
+      const pokemonId = 'non-existent-id';
+      await client.get(`/pokemon/${pokemonId}`).expect(404);
+    });
   });
 
-  it('invokes GET /pokemon/{id} with non-existent pokemon id', async () => {
-    const pokemonId = 'non-existent-id';
-    await client.get(`/pokemon/${pokemonId}`).expect(404);
-  });
+  describe('invokes GET /pokemon/name/{name}', () => {
+    it('returns an existent pokemon given a name', async () => {
+      const pokemonName = 'bulbasaur';
+      const res = await client.get(`/pokemon/name/${pokemonName}`).expect(200);
+      expect(res.body.name).to.eql('Bulbasaur');
+    });
 
-  it('invokes GET /pokemon/name/{name} existent pokemon name', async () => {
-    const pokemonName = 'bulbasaur';
-    const res = await client.get(`/pokemon/name/${pokemonName}`).expect(200);
-    expect(res.body.name).to.eql('Bulbasaur');
-  });
+    it('returns an existent existent pokemon given a name with case sensitive ', async () => {
+      const pokemonName = 'Bulbasaur';
+      const res = await client.get(`/pokemon/name/${pokemonName}`).expect(200);
+      expect(res.body.name).to.eql(pokemonName);
+    });
 
-  it('invokes GET /pokemon/name/{name} for an existent pokemon without case sensitive ', async () => {
-    const pokemonName = 'Bulbasaur';
-    const res = await client.get(`/pokemon/name/${pokemonName}`).expect(200);
-    expect(res.body.name).to.eql(pokemonName);
-  });
-
-  it('invokes GET /pokemon/name/{name} for a non existent pokemon name', async () => {
-    const pokemonName = 'Non-Existent-Pokemon';
-    await client.get(`/pokemon/name/${pokemonName}`).expect(404);
+    it('returns a not found status given a non existent pokemon name', async () => {
+      const pokemonName = 'Non-Existent-Pokemon';
+      await client.get(`/pokemon/name/${pokemonName}`).expect(404);
+    });
   });
 });
